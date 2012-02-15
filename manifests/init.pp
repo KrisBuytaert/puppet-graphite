e15a4945c57553c983d1ed73fe648b379e62d1b6
Kris Buytaert <sdog@inuits.be>
2012-02-15 23:30:11 +0100

    Can't make lint happier

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,31 +13,31 @@
 # [Remember: No empty lines between comments and class definition]
 class graphite {
 
-	include graphite::carbon
-	include graphite::whisper
-	include graphite::web
+  include graphite::carbon
+  include graphite::whisper
+  include graphite::web
 
 }
 
 
 class graphite::web {
 
-	     
-	package {
-		"bitmap-fonts-compat":	
-			ensure => present;
-		"graphite-web":
-			ensure => present;
-	}
+
+  package {
+    'bitmap-fonts-compat':
+      ensure => present;
+    'graphite-web':
+      ensure => present;
+  }
 
 
 
-        service { 'httpd':
-                ensure     => running,
-                enable     => true,
-                hasrestart => true,
-                hasstatus  => true,
-	}
+  service { 'httpd':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => true,
+  }
 
 
 }
@@ -46,9 +46,9 @@ class graphite::web {
 class graphite::whisper {
 
 
-	package {"whisper":
-		ensure => present;
-	}
+  package {'whisper':
+    ensure => present;
+  }
 
 
 
@@ -60,45 +60,46 @@ class graphite::whisper {
 class graphite::carbon {
 
 
-	package {"carbon":
-		ensure => present;
-	}
+  package {'carbon':
+    ensure => present;
+  }
 
 
 
 
 
-    	service { 'carbon':
-      		ensure     => running,
-      		enable     => true,
-      		hasrestart => true,
-      		hasstatus  => true,
-		require    => Package['carbon'],
-    }
+  service { 'carbon':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => true,
+    require    => Package['carbon'],
+  }
 
 
 }
 
-# needs to move to graphite::demo example and run example-client.py from the package   
+# needs to move to graphite::demo example and run
+# example-client.py from the package
 
 
 class graphite::demo {
 
 
- exec { "startClientExample":
-                command => "bash -c 'python /usr/share/doc/graphite-web-0.9.9/example-client.py >> /var/log/exampleclient.log' disown &",
-                require => [ Class["graphite::web"], Class["graphite::carbon"],  Class["graphite::whisper"]],
-                creates => "/var/log/exampleclient.log"
-        }
+  exec { 'startClientExample':
+    command => "bash -c 'python /usr/share/doc/graphite-web-0.9.9/example-client.py >> /var/log/exampleclient.log' disown &",
+    require => [ Class['graphite::web'], Class['graphite::carbon'],  Class['graphite::whisper']],
+    creates => '/var/log/exampleclient.log'
+  }
 }
 
 
-# Implement user creation 
+# Implement user creation
 
 #
 #
-#python manage.py shell 
-#Python 2.6.5 (r265:79063, Nov 12 2010, 00:52:45) 
+#python manage.py shell
+#Python 2.6.5 (r265:79063, Nov 12 2010, 00:52:45)
 #[GCC 4.4.4 20100525 (Red Hat 4.4.4-5)] on linux2
 #Type "help", "copyright", "credits" or "license" for more information.
 #(InteractiveConsole)
@@ -107,5 +108,5 @@ class graphite::demo {
 #>>> user.save()
 #>>> quit
 #Use quit() or Ctrl-D (i.e. EOF) to exit
-#>>> 
+#>>>
 
