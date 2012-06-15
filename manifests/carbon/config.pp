d202dc096545195f22cc16ff27f70825a2a5aaeb
Kris Buytaert <sdog@inuits.be>
2012-06-15 23:59:10 +0200

    Implemented the storage-schemas define using concat

--- manifests/carbon/config.pp
+++ manifests/carbon/config.pp
@@ -12,4 +12,18 @@
 #
 # [Remember: No empty lines between comments and class definition]
 class graphite::carbon::config {
+  include concat::setup
+  concat { '/etc/carbon/storage-schemas.conf':
+    group   => '0',
+    mode    => '0644',
+    owner   => '0',
+    notify  => Service['carbon'];
+  }
+  concat::fragment { 'header':
+    target  => '/etc/carbon/storage-schemas.conf',
+    order   => 0,
+    source  => 'puppet:///modules/graphite/storage-schemas.conf'
+  }
+
+
 }
---
+++ manifests/carbon/storage.pp
@@ -0,0 +1,9 @@
+define graphite::carbon::storage ( $pattern,$retentions){
+  concat::fragment {"$name":
+    target  => '/etc/carbon/storage-schemas.conf',
+    order   => 10,
+    content => template('graphite/storage-schemas.erb'),
+    notify  => Service['carbon']
+  }
+
+}
