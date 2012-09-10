8cab496545c76b585aca3105c8da73ba4d0bd052
Julien Pivotto <roidelapluie@inuits.eu>
2012-09-10 15:01:08 +0200

    fix double quoted string containing no variables

--- manifests/demo.pp
+++ manifests/demo.pp
@@ -19,7 +19,7 @@
 class graphite::demo {
 
   exec { 'startClientExample':
-    command => "bash -c 'python /usr/share/doc/graphite-web-0.9.9/example-client.py >> /var/log/exampleclient.log' disown &",
+    command => 'bash -c \'python /usr/share/doc/graphite-web-0.9.9/example-client.py >> /var/log/exampleclient.log\' disown &',
     require => [
       Class['graphite::web'],
       Class['graphite::carbon'],
