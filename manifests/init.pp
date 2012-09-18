837e2e36ab3f76f6d5c0e8a4bc9e14692de96c2d
Nathan Huff <nrhuff@umn.edu>
2012-09-18 14:49:02 -0500

    Make backward compatible with current version

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,28 +14,14 @@
 #
 # * Implement user creation.
 #
-class graphite(
-  $local_settings_file = 'UNSET',
-  $schema_file         = 'UNSET',
-  $time_zone           = 'UNSET'
-){
+class graphite
+{
 
-  include graphite::params
+  require graphite::params
 
-  $r_local_settings_file = $local_settings_file ? {
-    'UNSET' => $graphite::params::local_settings_file,
-    default => $local_settings_file
-  }
-
-  $r_schema_file = $schema_file ? {
-    'UNSET' => $graphite::params::schema_file,
-    default => $schema_file,
-  }
-
-  $r_time_zone = $time_zone ? {
-    'UNSET' => $graphite::params::time_zone,
-    default => $time_zone,
-  }
+  $r_local_settings_file = $graphite::params::local_settings_file
+  $r_schema_file         = $graphite::params::schema_file
+  $r_time_zone           = $graphite::params::time_zone
 
   include graphite::carbon
   include graphite::whisper
--- manifests/params.pp
+++ manifests/params.pp
@@ -10,9 +10,10 @@
 #
 # * Implement user creation.
 #
-class graphite::params {
-  $time_zone           = undef
-  $schema_file         = '/etc/carbon/storage-schemas.conf'
-  $local_settings_file = '/etc/graphite-web/local_settings.py'
-}
+class graphite::params (
+  $time_zone           = undef,
+  $schema_file         = '/etc/carbon/storage-schemas.conf',
+  $local_settings_file = '/etc/graphite-web/local_settings.py' ) {
+
+  }
 
