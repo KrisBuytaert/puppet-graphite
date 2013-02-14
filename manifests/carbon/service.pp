59cc078c5189d0c7a2e2f3ebce1783df4d5acb8e
Tom De Vylder <tom@penumbra.be>
2013-02-14 17:10:21 +0100

    Service name abstraction

--- manifests/carbon/service.pp
+++ manifests/carbon/service.pp
@@ -1,12 +1,17 @@
 # Class: graphite::carbon::service
 #
 class graphite::carbon::service {
-  service { 'carbon-cache':
+  $service_name = $::osfamily ? {
+    /(?i:Debian)/ => 'carbon-cache',
+    /(?i:RedHat)/ => 'carbon-cache',
+    default       => 'carbon-cache',
+  }
+
+  service { $service_name:
     ensure     => running,
     enable     => true,
     hasrestart => true,
     hasstatus  => true,
-    require    => Package['carbon'],
   }
 }
 
--- manifests/web/service.pp
+++ manifests/web/service.pp
@@ -1,7 +1,13 @@
 # Class: graphite::web::service
 #
 class graphite::web::service {
-  service { 'httpd':
+  $service_name = $::osfamily ? {
+    /(?i:Debian)/ => 'apache2',
+    /(?i:RedHat)/ => 'httpd',
+    default       => 'httpd',
+  }
+
+  service { $service_name:
     ensure     => running,
     enable     => true,
     hasrestart => true,
