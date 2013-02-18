72e2f6384374da9de74bee70c269447ddf3aedd2
Tom De Vylder <tom@penumbra.be>
2013-02-18 14:23:56 +0100

    Missing parameter: web::config_dir

--- manifests/web/params.pp
+++ manifests/web/params.pp
@@ -1,6 +1,12 @@
 # Class: graphite::web::params
 #
 class graphite::web::params {
+  $config_dir = $::osfamily ? {
+    /(?i:Debian)/ => '/opt/graphite/webapp/graphite',
+    /(?i:RedHat)/ => '/etc/graphite-web',
+    default       => '/etc/graphite-web',
+  }
+
   $service_name = $::osfamily ? {
     /(?i:Debian)/ => 'apache2',
     /(?i:RedHat)/ => 'httpd',
