aaa4c55fae9840ba677f6cff32f59616f3e0866a
bjanssens <bjanssens@inuits.eu>
2015-04-02 12:00:52 +0200

    Added the carbon_config_dir to  carbon.conf
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

--- manifests/carbon/config.pp
+++ manifests/carbon/config.pp
@@ -13,24 +13,24 @@
 # [Remember: No empty lines between comments and class definition]
 class graphite::carbon::config inherits graphite::carbon {
 
-  concat { "${carbon_config_dir}/storage-schemas.conf":
+  concat { "${carbon_config_dir}storage-schemas.conf":
     group   => '0',
     mode    => '0644',
     owner   => '0',
     notify  => Service['carbon-cache'],
   }
   concat::fragment { 'header':
-    target  => "${carbon_config_dir}/storage-schemas.conf",
+    target  => "${carbon_config_dir}storage-schemas.conf",
     order   => 0,
     source  => 'puppet:///modules/graphite/storage-schemas.conf'
   }
 
-  file { "${carbon_config_dir}/carbon.conf":
-    ensure => present,
-    group  => 'root',
-    owner  => 'root',
-    mode   => '0644',
-    source => 'puppet:///modules/graphite/carbon.conf'
+  file { "${carbon_config_dir}carbon.conf":
+    ensure  => present,
+    group   => 'root',
+    owner   => 'root',
+    mode    => '0644',
+    content => template('graphite/carbon/carbon.conf.erb'),
   }
 
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -13,7 +13,7 @@
 class graphite::params (
   $carbon_package           = 'python-carbon',
   $carbon_package_ensure  = 'present',
-  $carbon_config_dir      = '/etc/carbon',
+  $carbon_config_dir      = '/etc/carbon/',
   $time_zone                = 'UTC',
   $manage_httpd             = false,
   $basic_http_auth          = false,
