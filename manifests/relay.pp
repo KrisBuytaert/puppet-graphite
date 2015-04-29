9555fd1c37a89b31d339862462203ec19ca59e73
bjanssens <bjanssens@inuits.eu>
2015-04-29 15:59:06 +0200

    Bugfix: Set the relay_method right for the relay
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

--- manifests/relay.pp
+++ manifests/relay.pp
@@ -29,6 +29,7 @@
 #
 class graphite::relay (
   $relay_destinations                = undef,
+  $relay_method                      = 'consistent-hashing',
   $carbon_pickle_receiver_port       = $graphite::params::relay_pickle_receiver_port,
   $relay_carbon_metirc_interval      = $graphite::params::relay_carbon_metric_interval,
   $relay_carbon_metric_prefix        = $graphite::params::relay_carbon_metric_perfix,
