8f3650f40a4b1add878da536df8b5381c83f3aac
bjanssens <bjanssens@inuits.eu>
2015-05-04 12:02:26 +0200

    Make the lock file dependend on the exec
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

--- manifests/web/service.pp
+++ manifests/web/service.pp
@@ -4,14 +4,15 @@ class graphite::web::service () inherits ::graphite::web {
   $managesyncdb_path = "${web_dir}.DoNotDeletePlease"
 
   if !str2bool("$manualdbsetup") {
-    exec { 'setup db':
-      command  => '/usr/bin/python /usr/lib/python2.6/site-packages/graphite/manage.py syncdb --noinput',
-      creates  => $mangesyncdb_path,
+    exec { 'setup_db':
+      command => '/usr/bin/python /usr/lib/python2.6/site-packages/graphite/manage.py syncdb --noinput',
+      creates => $managesyncdb_path,
     }
 
     file { $managesyncdb_path:
-      ensure  => present,
-      content => 'This file is a block in puppet for the syncdb command'
+      ensure   => present,
+      content  => 'This file is a block in puppet for the syncdb command',
+      require  => Exec['setup_db'],
     }
   }
 
