057672e644171436eb18b481aa34d932b5d1b23f
bjanssens <bjanssens@inuits.eu>
2015-05-04 16:56:49 +0200

    Made auth class more generic
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

--- manifests/web.pp
+++ manifests/web.pp
@@ -25,18 +25,15 @@ class graphite::web (
   $cluster_servers          = undef,
   $time_zone                = $::graphite::params::web_time_zone,
   $whisper_dir              = $::graphite::params::whisper_dir,
-  $manage_httpd             = $::graphite::params::web_manage_httpd,
-  $basic_http_auth          = $::graphite::params::web_basic_http_auth,
-  $basic_http_auth_password = $::graphite::params::web_basic_http_auth_password,
   $graphite_web_port        = $::graphite::params::web_port,
   ) inherits ::graphite::params {
   contain graphite::web::package
   contain graphite::web::config
 
-  if str2bool("$manage_httpd") {
+  if str2bool("$web_manage_httpd") {
     include graphite::web::service
   }
-  if str2bool("$basic_http_auth") {
+  if str2bool("$web_basic_http_auth") {
     include graphite::web::auth
   }
 }
--- manifests/web/auth.pp
+++ manifests/web/auth.pp
@@ -11,25 +11,22 @@
 # Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
-class graphite::web::auth (
-  $basic_http_auth = false,
-  $basic_http_auth_password = undef,
-  ){
+class graphite::web::auth inherits graphite::web {
 
   file {'/etc/httpd/conf.d/graphite-web.conf':
     ensure    => file,
     owner     => 'root',
     group     => 'root',
     mode      => '0644',
-    notify    => Service['httpd'],
+    notify    => Service[$web_service_name],
     content   => template('graphite/web/graphite-web.conf.erb');
   }
 
-  file {'/usr/share/graphite/.htpasswd':
+  file {"${web_graphite_root}.htpasswd":
     ensure  => file,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => "inuits:${basic_http_auth_password}",
+    content => "${web_basic_http_user}:${web_basic_http_password}",
   }
 }
